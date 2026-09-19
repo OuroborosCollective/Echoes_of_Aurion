40b7e304c95f3843724b82aedcbaacf75c68cf41
Ouroboros Collective <projectouroboroscollective@gmail.com>
2026-09-19 21:25:10 +0200

    db(step27): extend schema control plane through 0054

--- server/aurionProductionSchemaReconcileDockerRunner.test.ts
+++ server/aurionProductionSchemaReconcileDockerRunner.test.ts
@@ -117,6 +117,7 @@ function makeArtifact() {
   "0051_aurion_cross_zone_handover_v2",
   "0052_aurion_effect_intent_journal",
   "0053_aurion_npc_action_gateway_v2",
+  "0054_aurion_semantic_memory_graph_v2",
   ];
   fs.mkdirSync(path.join(directory, "bin"), { recursive: true });
   fs.mkdirSync(path.join(directory, "drizzle"), { recursive: true });
