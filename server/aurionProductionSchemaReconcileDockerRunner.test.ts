41c194a30480de420b7a89509ce7f0753d6140bb
Ouroboros Collective <projectouroboroscollective@gmail.com>
2026-09-23 22:03:41 +0200

    test(db): include 0061 in reconcile artifact fixture

--- server/aurionProductionSchemaReconcileDockerRunner.test.ts
+++ server/aurionProductionSchemaReconcileDockerRunner.test.ts
@@ -124,6 +124,7 @@ function makeArtifact() {
   "0058_aurion_quest_causal_anchor",
     "0059_aurion_mobile_movement_mode",
     "0060_aurion_npc_policy_evolution",
+    "0061_aurion_exploration_memory",
   ];
   fs.mkdirSync(path.join(directory, "bin"), { recursive: true });
   fs.mkdirSync(path.join(directory, "drizzle"), { recursive: true });
